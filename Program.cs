1a3fb109cdb92466cc98c2a2c110d10c6c47c8bb
jeremygabillet <jeremy.gabillet@gmail.com>
2021-11-14 21:10:28 +0100

    Finishing quest

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace reflection
 {
@@ -6,7 +8,43 @@ namespace reflection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string i = "";
+            GetAllProperties(i);
+            GetAllFields(i);
+            GetAllMethods(i);
+        }
+        private static void GetAllProperties(object reflected)
+        {
+            Type objectType = reflected.GetType();
+            PropertyInfo[] propertyInfos = objectType.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic);
+            System.Console.WriteLine("Properties : ");
+            foreach (PropertyInfo propertyInfo in propertyInfos)
+            {
+                Console.WriteLine("\t" + propertyInfo);
+            }
+            System.Console.WriteLine("\n");
+        }
+        private static void GetAllFields(object reflected)
+        {
+            Type objectType = reflected.GetType();
+            FieldInfo[] fieldInfos = objectType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            System.Console.WriteLine("Fields : ");
+            foreach (FieldInfo fieldInfo in fieldInfos)
+            {
+                System.Console.WriteLine("\t" + fieldInfo);
+            }
+            System.Console.WriteLine("\n");
+        }
+        private static void GetAllMethods(object reflected)
+        {   
+            Type objectType = reflected.GetType();
+            MethodInfo[] methodInfos = objectType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            System.Console.WriteLine("Methods : ");
+            foreach (MethodInfo methodInfo in methodInfos)
+            {
+                System.Console.WriteLine("\t" + methodInfo);
+            }
+            System.Console.WriteLine("\n");
         }
     }
 }
